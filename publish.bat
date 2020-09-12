f7f284cf730e1f783a4815903f214e5e324fdf45
Deniansyah <5lineofcode@gmail.com>
2020-09-12 10:24:25 +0700

    Set version to pubver get

--- publish.bat
+++ publish.bat
@@ -2,6 +2,6 @@ set version=pubver get
 
 cmd /c pubver bump patch
 git add .
-git commit -m "Set version to $version"
+git commit -m "Set version to %version%"
 git push --force
 flutter pub publish /y
